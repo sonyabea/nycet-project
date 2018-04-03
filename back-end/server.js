d647d82fe00e5ce49eca729b962a50ee423752cb
Leandro Araneta <laraneta@ckmadvisors.com>
2018-04-02 22:57:11 -0400

    CONNECT TO SONYA'S DB chyeah

--- back-end/server.js
+++ back-end/server.js
@@ -1,16 +1,43 @@
 'use strict';
 
 const express = require('express');
+const pgp = require('pg-promise')();
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const cnxn = {
+    'user': 'nycetmember',
+    'password': 'J4}83,?{6X4$',
+    'host': '10.39.38.14',
+    'port': '5432',
+    'database': 'probono'
+}
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+
+    const db = pgp(cnxn);
+    db.connect()
+    .then(obj => {
+        console.log('poopdypants');
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
+
+//   res.send('Hello world\n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+
+
+
+
+
