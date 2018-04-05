7558bb1c7edcd10a19fc62e196067eb55e5d65fc
Leandro Araneta <laraneta@ckmadvisors.com>
2018-04-05 19:56:25 -0400

    connected frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,35 @@ const cnxn = {
 
 // App
 const app = express();
-app.get('/', (req, res) => {
 
+const db = pgp(cnxn);
+
+// function pingDB(query){
+
+// }
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.get('/', function(req, res) {
+    // Handle the get for this route
+    // debugger
+    // let params = req.params;
     const db = pgp(cnxn);
-    db.connect()
-    .then(obj => {
-        console.log('poopdypants');
-        obj.done(); // success, release the connection;
-    })
-    .catch(error => {
-        console.log('ERROR:', error.message || error);
-    });
+    let query = "SELECT * FROM acs_ed_demographics LIMIT 10"
+    db.query(query, [true])
+        .then(data => {
+            console.log('DATA:', data);
+        })
+        .catch(error => {
+            console.log(error)
+        })
 
-//   res.send('Hello world\n');
-});
+  res.send('Hello world\n');
+  });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
