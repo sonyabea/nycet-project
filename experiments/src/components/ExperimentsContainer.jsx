966d06902db14663d66998c797a09125e15ac081
Leandro Araneta <laraneta@ckmadvisors.com>
2018-04-11 12:40:10 -0400

    stuff

--- experiments/src/components/ExperimentsContainer.jsx
+++ experiments/src/components/ExperimentsContainer.jsx
@@ -3,21 +3,21 @@ import SelectionContainer from './SelectionContainer'
 import PlotContainer from './PlotContainer'
 import axios from 'axios'
 
-function withData (WrappedComponent, queryStuff) {
+function withData (WrappedComponent, {table, filters}) {
 
   return class extends Component {
 
     getData (data) {
       axios({
         method:'post',
-        url: `localhost:8080/table/${queryStuff.table}/`,
+        url: `localhost:8080/table/${table}/`,
         data
       })
     }
   
     componentDidMount () {
-      let attributes = Object.keys(queryStuff.filters)
-      let queries = Object.values(queryStuff.filters).map(getData)
+      let attributes = Object.keys(filters)
+      let queries = Object.values(filters).map(getData)
   
       Promise.all(queries)
         .then(values => {
@@ -36,6 +36,9 @@ class ExperimentsContainer extends Component {
 
   constructor (props) {
     super(props)
+    this.state = {
+      filters: ''
+    }
   }
 
   render () {
@@ -63,7 +66,7 @@ class ExperimentsWithGroupSizes extends Component {
   render () {
     return (
       <ExperimentsContainer { ...this.props }>
-        {/* <OtherThing /> */}
+        <GroupSizes />
       </ExperimentsContainer>
     )
   }
@@ -81,9 +84,14 @@ const experimentsByOrgParams = {
 }
 
 const demographicStatsParams = {
-  orgs: {'unique': true, 'columns': ['org']},
-  elections: {'unique': true, 'columns': ['election']},
-  dems: {'unique': true, 'columns': ['dem1']},
+  filters: {
+    elections: {'unique': true, 'columns': ['election']},
+    dems1: {'unique': true, 'columns': ['dem1']},
+    dems2: {'unique': true, 'columns': ['dem2']},
+    plotInfo: {'columns': ['org', 'election', 'contact_rate', 'CACE', 'control', 'treatment']},
+    plotStats: {'columns': ['org', 'election', 'q1', 'q3', 'ci_low', 'ci_high', 'median']}
+  },
+  table: 'cace_metrics'
 }
 
 export const ExperimentsByOrg = withData(ExperimentsContainer, experimentsByOrgParams)
