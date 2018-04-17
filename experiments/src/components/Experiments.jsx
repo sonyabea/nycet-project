11d617a3c211b8a667bb1a438685944e7089fb88
Leandro Araneta <laraneta@ckmadvisors.com>
2018-04-17 15:26:28 -0400

    not relating to tooltip at all

--- experiments/src/components/Experiments.jsx
+++ experiments/src/components/Experiments.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 // import { Loader } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { ExperimentsDropdownContainer, DemographicsDropdownContainer } from './DropdownContainer'
