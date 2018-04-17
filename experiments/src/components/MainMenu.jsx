7a16140529eebd4af7551958d07c9ff3d65a1381
Diane Cai <dcai@dc-c02tf2s3h3qf.local>
2018-04-16 21:43:59 -0400

    make header look more responsive

--- experiments/src/components/MainMenu.jsx
+++ experiments/src/components/MainMenu.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Menu, Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
-const MainMenu = ({linksInfo}) => {
-  let menuItems = linksInfo.map(l =>
-    <Menu.Item key={l.name} name={l.name} as={Link} to={l.path}>
-      {l.name}
-    </Menu.Item>
-  )
-  return <Segment inverted><Menu inverted pointing secondary size='massive'>{menuItems}</Menu></Segment>
+class MainMenu extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      activeItem: this.props.linksInfo[0]
+    }
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    let menuItems = this.props.linksInfo.map(l =>
+      <Menu.Item key={l.name}
+        name={l.name}
+        as={Link}
+        to={l.path}
+        active={this.state.activeItem === l.name}
+        onClick={this.handleItemClick}
+      >
+        {l.name}
+      </Menu.Item>
+    )
+
+    return  (
+      <Segment inverted>
+        <Menu inverted pointing secondary size='massive'>{menuItems}</Menu>
+      </Segment>
+    )
+  }
+
 }
 
 export default MainMenu
