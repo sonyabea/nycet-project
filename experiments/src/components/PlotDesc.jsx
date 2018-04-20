ef6b6ad502d343234ca4d957540735d11e90d703
Diane Cai <dcai@dc-c02tf2s3h3qf.local>
2018-04-20 16:14:34 -0400

    boxplot how-to-read hovers work

--- experiments/src/components/PlotDesc.jsx
+++ experiments/src/components/PlotDesc.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { VictoryChart, VictoryBoxPlot } from 'victory'
+import { VictoryChart, VictoryBoxPlot, VictoryTooltip } from 'victory'
 
 class PlotDesc extends Component {
   constructor(props) {
@@ -7,27 +7,40 @@ class PlotDesc extends Component {
   }
 
   render(){
-    // let protoEvent = (target) => {
-    //   target: target,
-    //   eventHandlers: {
-    //     onMouseOver: () => ({ target: `${target}Labels`, mutation: () => ({ active: true }) }),
-    //     onMouseOut: () => ({ target: `${target}Labels`, mutation: () => ({ active: false }) })
-    //   }
-    // }
-    //
-    // let targets = ['max', 'q3', 'median', 'q1', 'min']
-    // data={this.props.data} viewbox="0,0, 100%, 100%"
-    //   events:{events}
-    var atts = {
-      data: [{ x: 1, min: 2, median: 5, max: 8, q1: 3, q3: 7 }],
-      // events: targets.map(t => protoEvent(t))
+    const protoEvent = (target) => {
+      return (
+      {
+        'target': target,
+        'eventHandlers': {
+          onMouseOver: () => ({ target: `${target}Labels`, mutation: () => ({ active: true }) }),
+          onMouseOut: () => ({ target: `${target}Labels`, mutation: () => ({ active: false }) })
+        }
+      })
     }
 
+    const targets = ['max', 'q3', 'median', 'q1', 'min']
+
+    var atts = {
+      data: [{ x: 1, min: 2, median: 5, max: 8, q1: 3, q3: 7 }],
+      events: targets.map(t => protoEvent(t))
+    }
+
+    for (var t=0;t<targets.length;t++) {
+      let target = targets[t]
+      atts[`${target}Labels`] = `hot take on ${target}`
+      atts[`${target}LabelComponent`] = <
+        VictoryTooltip style={{fontSize: 30}}
+        orientation="right"
+        pointerLength={0}
+        />
+    }
 
 
     return (
       <div style={{height: '40%'}}>
+        <h6>Note: HOVER OVER ME!</h6>
         <VictoryBoxPlot {...atts}/>
+
       </div>
     )
   }
