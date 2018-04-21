afeb2e4149361f077255fe0850794d063c2a65e1
Diane Cai <dcai@dc-c02tf2s3h3qf.local>
2018-04-20 22:40:03 -0400

    refactor master

--- experiments/src/components/PlotDesc.jsx
+++ experiments/src/components/PlotDesc.jsx
@@ -1,65 +1,60 @@
-import React, { Component } from 'react'
-import { VictoryChart, VictoryBoxPlot, VictoryTooltip, VictoryLabel, VictoryGroup } from 'victory'
+import React from 'react'
+import { VictoryBoxPlot, VictoryTooltip, VictoryLabel, VictoryGroup } from 'victory'
 
-class PlotDesc extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render(){
-    const changeColorAndShowTooltip = (target) => {
-      return (
-      {
-        'target': target,
-        'eventHandlers': {
-          onMouseOver: () => {
-            return [
-              { target: `${target}Labels`, mutation: () => ({ active: true }) },
-              { target: target, mutation: () => ({ style: {stroke: 'red', fill: 'grey'} }) }
-            ]},
-          onMouseOut: () => {
-            return [
-              { target: `${target}Labels`, mutation: () => ({ active: false }) },
-              { target: target, mutation: () => ({ style: {stroke: target.includes('q') ? 'white': 'black', fill: 'grey'} }) }
-            ]}
-        }
-      })
-    }
-
-    const targets = ['max', 'q3', 'median', 'q1', 'min']
-
-    var atts = {
-      data: [{ x: 1, min: 2, median: 5, max: 8, q1: 3, q3: 7 }],
-      events: targets.map(t => changeColorAndShowTooltip(t)),
-      boxWidth: 90,
-    }
-
-    for (var t=0;t<targets.length;t++) {
-      let target = targets[t]
-      atts[`${target}Labels`] = `hot take on ${target}`
-      atts[`${target}LabelComponent`] =
-      <VictoryTooltip
-        style={{fontSize: 40}}
-        orientation="right"
-        pointerLength={0}
-      />
-    }
-
-
+const PlotDesc = () => {
+  const changeColorAndShowTooltip = (target) => {
     return (
-      <div style={{height: '35%', textAlign: 'center', 'verticalAlign': 'middle',
-        border: '1px dashed grey', borderRadius: '5px', padding: '5%'}}>
-        <div>
-          <VictoryGroup height={500} padding={{top:80, bottom: 50}}>
-            <VictoryLabel text="How to Read a Box Plot" dx={30} dy={30} style={{fontSize: 40}}/>
-            <VictoryBoxPlot {...atts}/>
-            <VictoryLabel text="Hover over me!" dx={120} dy={490} style={{fontSize:30}}/>
-          </VictoryGroup>
-
-        </div>
-      </div>
-    )
+    {
+      'target': target,
+      'eventHandlers': {
+        onMouseOver: () => {
+          return [
+            { target: `${target}Labels`, mutation: () => ({ active: true }) },
+            { target: target, mutation: () => ({ style: {stroke: 'red', fill: 'grey'} }) }
+          ]},
+        onMouseOut: () => {
+          return [
+            { target: `${target}Labels`, mutation: () => ({ active: false }) },
+            { target: target, mutation: () => ({ style: {stroke: target.includes('q') ? 'white': 'black', fill: 'grey'} }) }
+          ]}
+      }
+    })
   }
+
+  const addLabelAtt = (atts, target) => (
+    {...atts,
+      [`${target}Labels`]: `hot take on ${target}`,
+      [`${target}LabelComponent`]:
+        <VictoryTooltip
+          style={{fontSize: 40}}
+          orientation="right"
+          pointerLength={0}
+        />
+    }
+  )
+
+  const targets = ['max', 'q3', 'median', 'q1', 'min']
+
+  var atts = {
+    data: [{ x: 1, min: 2, median: 5, max: 8, q1: 3, q3: 7 }],
+    events: targets.map(changeColorAndShowTooltip),
+    boxWidth: 90,
+  }
+
+  var allAtts = targets.reduce(addLabelAtt, atts)
+
+  return (
+    <div style={{height: '35%', textAlign: 'center', 'verticalAlign': 'middle',
+      border: '1px dashed grey', borderRadius: '5px', padding: '5%'}}>
+      <div>
+        <VictoryGroup height={500} padding={{top:80, bottom: 50}}>
+          <VictoryLabel text="How to Read a Box Plot" dx={30} dy={30} style={{fontSize: 40}}/>
+          <VictoryBoxPlot {...atts}/>
+          <VictoryLabel text="Hover over me!" dx={120} dy={490} style={{fontSize:30}}/>
+        </VictoryGroup>
+      </div>
+    </div>
+  )
 
 }
 
