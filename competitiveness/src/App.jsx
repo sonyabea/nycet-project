c523f8a7d4522512574994e70d8509e0504a2921
Leandro Araneta <laraneta@ckmadvisors.com>
2018-04-21 18:56:00 -0400

    works jankily

--- competitiveness/src/App.jsx
+++ competitiveness/src/App.jsx
@@ -2,31 +2,28 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Competitiveness from './components/Competitiveness'
 import {loadData} from './actions/index'
+import { withRouter } from 'react-router-dom';
 import './App.css';
 
 //can add more complexity here in the children eventually
 class AppContainer extends Component {
-  componentWillMount() {
-    let parentDistType = (typeof(this.props.match.params.parentDistType) === 'undefined') ? 'AD' : this.props.match.params.parentDistType 
-    let parentDistId = (typeof(this.props.match.params.parentDistId) === 'undefined') ? 0 : this.props.match.params.parentDistId
-    this.props.loadMap({parentDistType, parentDistId})
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let parentDistType = (typeof(nextProps.match.params.parentDistType) === 'undefined') ? 'AD' : nextProps.match.params.parentDistType 
+    let parentDistId = (typeof(nextProps.match.params.parentDistId) === 'undefined') ? 0 : nextProps.match.params.parentDistId
+    nextProps.loadMap({parentDistType, parentDistId})
+    return null
   }
 
   render() {
-  return (
-    <div className='App'>
-      <Competitiveness mapComponents={this.props.mapData}
-                       districtType={this.props.districtType} />
-    </div>
-    )
+    return (
+      <div className='App'>
+        <Competitiveness />
+      </div>
+      )
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  mapData: state.mapData,
-  districtType: state.districtType
-})
-
-const App = connect(mapStateToProps, { loadMap: loadData })(AppContainer)
+const App = withRouter(connect(null, { loadMap: loadData })(AppContainer))
 
 export default App
