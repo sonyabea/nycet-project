cb7877a6b0caf77ab03d0ac8c2ab6090c16e45ba
Leandro Araneta <laraneta@ckmadvisors.com>
2018-05-07 16:51:26 -0400

    i am perfectionist scum

--- experiments/src/selectors/index.js
+++ experiments/src/selectors/index.js
@@ -69,7 +69,7 @@ const deriveDropdownOptions = (data, selected) => selected.reduce(
 			.map(d => ({key: d, text: format(d), value: d})) // format for semanticUI
 			.value()
 		let largestOptionLength = _.maxBy(newDropdownOptions, o => o.text.length).text.length
-		let width = (7.5*largestOptionLength + 40) + 'px'
+		let width = (7*largestOptionLength + 45) + 'px'
 		return {
 			data: _.filter(currentData, {[b.name]: b.selected}),
 			dropdownOptions: [
